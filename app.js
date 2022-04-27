a7a8284a17d39fe513fe295e9f115be970f75837
Oscar Gutierrez <oscar_yo_123@hotmail.com>
2022-04-27 15:06:18 -0500

    Agregando el metodo DELETE

--- app.js
+++ app.js
@@ -42,3 +42,9 @@ app.put("/v1/explorers/:id", (req, res) => {
   const requestBody = req.body; //Parámetros de un cliente
   res.status(200).json({ message: "Updated" });
 });
+
+app.delete("/v1/explorers/:id", (req, res) => {
+  console.log(`Api Explorers DELETE request ${new Date()}`);
+  console.log(`Delete explorer with id ${req.params.id}`);
+  res.status(200).json({ message: "Deleted" });
+});
