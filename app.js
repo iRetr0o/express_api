83a913fd0dfc543feabece7b3b4beb65b854a0f8
Oscar Gutierrez <oscar_yo_123@hotmail.com>
2022-04-26 20:14:39 -0500

    Agregando el endpoints de GET

--- app.js
+++ app.js
@@ -10,3 +10,15 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+//HTTP METHODS
+app.get("/v1/explorers", (req, res) => {
+  console.log(`Api Explores GET ALL request ${new Date()}`);
+  const explorer1 = { id: 1, name: "Oscar" };
+  const explorer2 = { id: 2, name: "Fernadna" };
+  const explorer3 = { id: 3, name: "Rodrigo" };
+  const explorer4 = { id: 4, name: "Carlo" };
+  const explorer5 = { id: 5, name: "Oscar" };
+  const explorers = [explorer1, explorer2, explorer3, explorer4, explorer5];
+  res.status(200).json(explorers);
+});
